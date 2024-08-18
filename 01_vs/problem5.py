8c211a3d8b58750894b416a7663d883a6933d70a
Sus-Hant1110 <nsushaant72@gmail.com>
2024-08-18 10:14:54 +0545

    first

--- 01_vs/problem5.py
+++ 01_vs/problem5.py
@@ -1,2 +1,10 @@
+# Problem 5:
+# WAP to Find the largest item from a given list using function.
 
+# Solution:
+def largest_item(list):
+    return max(list)
+
+list = [1, 2, 3, 4, 5]
+print("Largest item in the list is:", largest_item(list))
 
