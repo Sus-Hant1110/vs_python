163f9be3f56e3f16984e3a5f239807098c64fefd
Sus-Hant1110 <sushantneupane@divyagyan.edu.np>
2024-09-03 19:57:39 +0545

    donee

---
+++ 02_vs/05_problem.py
@@ -0,0 +1,12 @@
+# program that calculates price of a product after discount
+# discount is in percentage
+
+def calculate_price(price, discount):
+    return price - (price * discount / 100)
+
+price = float(input("Enter price of product: "))
+discount = float(input("Enter discount percentage: "))
+print("Price after discount: ", calculate_price(price, discount))
+
+
+
